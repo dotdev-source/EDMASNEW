80360ed285abf3b98bad4d073048992770096e1b
“Xaxxoo” <bnahmad83@gmail.com>
2023-08-26 02:18:29 +0100

    pagination implemented

--- controller/staff/teachersControllers.js
+++ controller/staff/teachersControllers.js
@@ -65,11 +65,44 @@ exports.teachersLogin = asyncHandler(async (req, res) => {
 // access = Private
 
 exports.getAllTeachers = asyncHandler(async (req, res) => {
-  const teachers = await Teacher.find();
+  //convert query string to number
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  //Get Total Record
+  const totalTeachers = await Teacher.countDocuments();
+  const startIndex = (page -1 ) * limit
+  const endIndex = page * limit;
+
+  //pagination result
+  const pagination = {};
+
+ // add next
+  if (endIndex < totalTeachers) {
+    pagination.next ={
+      page: page + 1,
+      limit
+    }
+  }
+
+  //Add previous
+  if (startIndex > 0) {
+    pagination.prev ={
+      page: page - 1,
+      limit
+    }
+  }
+
+  //Execute Query
+  const teachers = await Teacher.find().skip(skip).limit(limit);
 
   res.status(200).json({
+    totalTeachers,
+    pagination,
     status: "success",
     message: "Teachers fetched successfully",
+    result: teachers.length,
     data: teachers,
   });
 });
