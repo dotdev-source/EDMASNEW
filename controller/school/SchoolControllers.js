a7d83eea0139f1f4f4f13d1e8124a1f86263be95
“Xaxxoo” <bnahmad83@gmail.com>
2023-09-04 21:44:11 +0100

    School controllers

---
+++ controller/school/SchoolControllers.js
@@ -0,0 +1,97 @@
+const asyncHandler = require("express-async-handler");
+const School = require("../../models/school/School");
+
+// Register School
+// Route POST api/schools/register
+// access = Private
+const registerSchool = asyncHandler(async (req, res) => {
+  const { schoolName, schoolType, regNo, schoolAddress } = req.body;
+
+  //check if School exist
+  const schoolExist = await School.findOne({ email });
+  if (schoolExist) {
+    throw new Error("School Already Exist");
+  }
+
+  //Register School
+  const school = await School.create({
+    schoolName,
+    schoolType,
+    regNo,
+    schoolAddress,
+  });
+  res.status(201).json({
+    status: "success",
+    data: school,
+    message: "School created successfully",
+  });
+});
+
+
+
+// Get All School
+// Route GET api/schools/
+// access = Private
+const allSchools = asyncHandler(async (req, res) => {
+    schools = await School.find()
+
+    res.status(200).json({
+        status: 'success',
+        data: schools,
+        message: 'Schools fetched successfully'
+    })
+});
+
+// Get Single School
+// Route GET api/schools/:id
+// access = Private
+const getSchoolProfileCtrl = asyncHandler(async (req, res) => {
+  const school = await School.findById(req.userAuth._id)
+    .select("-password")
+    .populate("academicYears");
+  if (!school) {
+    throw new Error("School not found");
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: school,
+      message: "School Profile fetched succcessfully",
+    });
+  }
+});
+
+// Update School
+// Route Put api/schools/:id
+// access = Private
+const updateSchool = asyncHandler(async (req, res) => {
+  const { schoolName, schoolType, regNo, schoolAddress } = req.body;
+
+  const schoolNameExist = await School.findOne({ schoolNameExist });
+  if (emailExist) {
+    throw new Error("School not found");
+  }
+    const school = await School.findByIdAndUpdate(
+      req.userAuth._id,
+      { schoolName, schoolType, regNo, schoolAddress },
+    );
+    res.status(200).json({
+      status: "success",
+      data: school,
+      message: "School updated successfully",
+    });
+  }
+);
+
+// Delete School
+// Route DELETE api/schools/:id
+// access = Private
+
+
+
+module.exports = {
+  registerSchool,
+  allSchools,
+  getSchoolProfileCtrl,
+  updateSchool,
+  deleteSchool,
+};
